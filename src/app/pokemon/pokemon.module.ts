82695ca8210632af08896c3f543265b1da0047d9
Iana_Avramenko@epam.com <AvramenkoYana@yandex.ru>
2020-03-22 20:55:36 +0300

    generating components - header, pokemon-card and pokemon-list

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
+import { PokemonsListComponent } from './pokemons-list/pokemons-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, PokemonCardComponent, PokemonsListComponent],
   imports: [
     CommonModule
   ]
