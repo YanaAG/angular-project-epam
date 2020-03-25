0d9a0507618bffe7fe34be53c3d871a84f0ec698
Iana_Avramenko@epam.com <AvramenkoYana@yandex.ru>
2020-03-25 19:33:59 +0300

    displaying a list of pokemons in the form of cards

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -3,13 +3,21 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
 import { PokemonsListComponent } from './pokemons-list/pokemons-list.component';
-
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
   declarations: [HeaderComponent, PokemonCardComponent, PokemonsListComponent],
+  exports: [
+    PokemonsListComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatGridListModule,
+    MatButtonModule
   ]
 })
 export class PokemonModule { }
--- src/app/pokemon/pokemons-list/pokemons-list.component.ts
+++ src/app/pokemon/pokemons-list/pokemons-list.component.ts
@@ -1,12 +1,79 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Pokemon {
+  id: number;
+  name: string;
+  damage: number;
+}
+
 @Component({
   selector: 'app-pokemons-list',
   templateUrl: './pokemons-list.component.html',
   styleUrls: ['./pokemons-list.component.css']
 })
 export class PokemonsListComponent implements OnInit {
-
+  pokemons: Pokemon[] = [
+    {
+      id: 1,
+      name: 'Bulbasaur',
+      damage: 15,
+    },
+    {
+      id: 4,
+      name: 'Charmander',
+      damage: 15,
+    },
+    {
+      id: 7,
+      name: 'Squirtle',
+      damage: 15,
+    },
+    {
+      id: 32,
+      name: 'Nidoran',
+      damage: 15,
+    },
+    {
+      id: 39,
+      name: 'Jigglypuff',
+      damage: 15,
+    },
+    {
+      id: 116,
+      name: 'Horsea',
+      damage: 15,
+    },
+    {
+      id: 153,
+      name: 'Bayleef',
+      damage: 15,
+    },
+    {
+      id: 164,
+      name: 'Noctowl',
+      damage: 15,
+    },
+    {
+      id: 172,
+      name: 'Pichu',
+      damage: 15,
+    },
+    {
+      id: 182,
+      name: 'Bellossom',
+      damage: 15,
+    },
+    {
+      id: 261,
+      name: 'Poochyena',
+      damage: 15,
+    },
+    {
+      id: 300,
+      name: 'Skitty',
+      damage: 15,
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
