60153990870771a0305271e9484ff5eb103464ce
Iana_Avramenko@epam.com <AvramenkoYana@yandex.ru>
2020-04-05 22:08:22 +0300

    creating a pokemon-search component

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -10,10 +10,12 @@ import { BorderDirective } from '../border.directive';
 import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { PokemonSearchComponent } from './pokemon-search/pokemon-search.component';
+import {MatInputModule} from '@angular/material/input';
 
 
 @NgModule({
-  declarations: [HeaderComponent, PokemonCardComponent, PokemonsListComponent, BorderDirective],
+  declarations: [HeaderComponent, PokemonCardComponent, PokemonsListComponent, BorderDirective, PokemonSearchComponent],
   exports: [
     PokemonsListComponent,
     HeaderComponent
@@ -25,7 +27,8 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     MatButtonModule,
     MatListModule,
     MatIconModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatInputModule
   ]
 })
 export class PokemonModule { }
