8f3c1ad91ab926f13b43414630e3c11e531f4c42
Iana_Avramenko@epam.com <AvramenkoYana@yandex.ru>
2020-04-13 12:01:53 +0300

    adding search parameters to queryParams

--- src/app/pokemon/pokemon-search/pokemon-search.component.ts
+++ src/app/pokemon/pokemon-search/pokemon-search.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Pokemon, PokemonService} from '../pokemon.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-pokemon-search',
@@ -9,20 +10,36 @@ import {Pokemon, PokemonService} from '../pokemon.service';
 export class PokemonSearchComponent implements OnInit {
   pokemonName = '';
   @Output() filterPokemons = new EventEmitter<Pokemon[]>();
-  constructor(private pokemonService: PokemonService) { }
+  constructor(private pokemonService: PokemonService, private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    if (this.activatedRoute.snapshot.queryParams.pokemonName) {
+      this.pokemonName = this.activatedRoute.snapshot.queryParams.pokemonName;
+      this.findPokemon();
+    }
   }
   findPokemon(): void {
     const pokemon = this.pokemonService.filter(this.pokemonName);
+    this.addParameter(this.pokemonName);
     this.filter(pokemon);
   }
   clearInput(): void {
     this.pokemonName = '';
     const pokemons = this.pokemonService.getAll();
+    this.addParameter(null);
     this.filter(pokemons);
   }
   filter(pokemon: Pokemon[]): void {
     this.filterPokemons.emit(pokemon);
+  }
+  addParameter(param: string): void {
+    this.router.navigate(
+      [],
+      {
+        relativeTo: this.activatedRoute,
+        queryParams: {pokemonName: param},
+        queryParamsHandling: 'merge'
+      }
+    );
   }
 }
