64491de2cda9752ac7060e7711d3d67ad0ff76bb
Iana_Avramenko@epam.com <AvramenkoYana@yandex.ru>
2020-04-15 09:52:21 +0300

    added the creation date to the pokemon object and modified pokemon detailed card

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -4,6 +4,7 @@ export interface Pokemon {
   id: number;
   name: string;
   damage: number;
+  creationDate: Date;
 }
 
 @Injectable({
@@ -15,61 +16,73 @@ export class PokemonService {
       id: 1,
       name: 'Bulbasaur',
       damage: 15,
+      creationDate: new Date('December 17, 2020'),
     },
     {
       id: 4,
       name: 'Charmander',
       damage: 25,
+      creationDate: new Date('December 25, 2020'),
     },
     {
       id: 7,
       name: 'Squirtle',
       damage: 35,
+      creationDate: new Date('December 10, 2020'),
     },
     {
       id: 32,
       name: 'Nidoran',
       damage: 45,
+      creationDate: new Date('October 5, 2020'),
     },
     {
       id: 39,
       name: 'Jigglypuff',
       damage: 55,
+      creationDate: new Date('October 15, 2020'),
     },
     {
       id: 116,
       name: 'Horsea',
       damage: 10,
+      creationDate: new Date('October 29, 2020'),
     },
     {
       id: 153,
       name: 'Bayleef',
       damage: 20,
+      creationDate: new Date('April 2, 2020'),
     },
     {
       id: 164,
       name: 'Noctowl',
       damage: 30,
+      creationDate: new Date('April 21, 2020'),
     },
     {
       id: 172,
       name: 'Pichu',
       damage: 40,
+      creationDate: new Date('April 9, 2020'),
     },
     {
       id: 182,
       name: 'Bellossom',
       damage: 50,
+      creationDate: new Date('September 1, 2020'),
     },
     {
       id: 261,
       name: 'Poochyena',
       damage: 60,
+      creationDate: new Date('September 15, 2020'),
     },
     {
       id: 300,
       name: 'Skitty',
       damage: 70,
+      creationDate: new Date('September 28, 2020'),
     }
   ];
   constructor() { }
